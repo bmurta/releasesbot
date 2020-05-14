8772c6ac54f39b9ea96e6a700657fdb61a75735f
Bruno Murta <b.murta.sampaio@gmail.com>
2020-05-14 01:05:27 -0300

    feat: add command variables and args-info command

--- bot.js
+++ bot.js
@@ -11,13 +11,31 @@ client.on("ready", () => {
 client.login(token);
 
 client.on("message", (msg) => {
-	if (msg.content.startsWith(`${prefix}ping`)) {
-      msg.reply("pong");
-	}
-	else if (msg.content.startsWith(`${prefix}ping`)) {
-      msg.reply("boop");
-	}
-    else if (msg.content.startsWith(`${prefix}nome`)) {
-      msg.reply(`O nominho do servidor é: ${msg.guild.name}, ele tem ${msg.guild.memberCount} membros\nFoi criado em ${msg.guild.createdAt}, e é do ${msg.guild.region}`);
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command === "args-info") {
+    if (!args.length) {
+      return msg.channel.send(`Cade os argumento??, ${msg.author}!`);
+    } else if (args[0] === "foo") {
+      msg.channel.send("bar");
+    }
+    msg.channel.send(`O comando foi: ${command}\nArgumento: ${args}`);
+  }
+
+  if (command === "ping") {
+    msg.reply("pong");
+  }
+
+  if (command === "beep") {
+    msg.reply("boop");
+  }
+
+  if (command === "nome") {
+    msg.reply(
+      `O nominho do servidor é: ${msg.guild.name}, ele tem ${msg.guild.memberCount} membros\nFoi criado em ${msg.guild.createdAt}, e é do ${msg.guild.region}`
+    );
   }
 });
