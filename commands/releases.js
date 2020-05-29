478d365f252709bf7b18b1efb4f38d21dc3e8d3b
bruno.murta <b.murta.sampaio@gmail.com>
2020-05-29 14:59:57 -0300

    finish releases command

--- commands/releases.js
+++ commands/releases.js
@@ -1,28 +1,43 @@
-var moment = require('moment-timezone');
+var moment = require("moment-timezone");
 
 const axios = require("axios").default;
 const { IGDB_KEY } = require("../token.json");
 
-console.log(moment().tz("America/Bahia").format())
+let now = moment().tz("America/Bahia").valueOf();
+console.log(now);
 module.exports = {
-    name: "releases",
-    description: "Returns todays Releases for the console",
-    execute(msg, args) {
-        if (!args.length) {
-            return msg.reply(`What console?`);
-      } else {
-        var con = args.toString().replace(/,/g, " ");
-        axios({
-            url: "https://api-v3.igdb.com/platforms",
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              "user-key": IGDB_KEY,
-            },
-            data: `fields *; search "${con}";`,
-        }).then(response  => {
+  name: "releases",
+  description: "Returns todays Releases for the console",
+  execute(msg, args) {
+    if (!args.length) {
+      return msg.reply(`What console?`);
+    } else {
+      var con = args.toString().replace(/,/g, " ");
+      axios({
+        url: "https://api-v3.igdb.com/platforms",
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "user-key": IGDB_KEY,
+        },
+        data: `fields *; search "${con}";`,
+      }).then(response => {
+        const id = response.data[0].id;
+        const name = response.data[0].name
 
-        })
-      }
+        axios({
+          url: "https://api-v3.igdb.com/games",
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "user-key": IGDB_KEY,
+          },
+          data: `fields *; where platforms = ${id} & release_dates = ${now}; sort date desc;`,
+        }).then(response => {
+            console.log(response.data[0])
+            return msg.reply(`Todays releases for the ${name} are:\n ${response.data[0].name}`)
+        });
+      });
+    }
   },
 };
