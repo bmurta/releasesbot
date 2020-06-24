7c95fb7c75e4945a69355ce1f8e15c346413faaf
bruno.murta <b.murta.sampaio@gmail.com>
2020-06-24 12:11:06 -0300

    timetobeat now sends correctly

--- commands/timetobeat.js
+++ commands/timetobeat.js
@@ -10,9 +10,14 @@ module.exports = {
     } else {
       var gam = args.toString().replace(/,/g, " ");
       hltbService.search(gam).then((response) => {
-		console.log(response);
-		const main = response.data[0].gameplayMain;
-		console.log(main)
+        console.log(response);
+        var main = response[0].gameplayMain,
+          extra = response[0].gameplayMainExtra,
+          completionist = response[0].gameplayCompletionist,
+          name = response[0].name;
+        return msg.channel.send(
+          `The average times to beat ${name} are:\n Main Story: ${main} hours\n Main Story + Extras: ${extra} hours\n Completionist: ${completionist} hours`
+        );
       });
     }
   },
