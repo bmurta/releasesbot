198dddce8bf0baee71469318a7a54ef2197bc30a
bruno.murta <b.murta.sampaio@gmail.com>
2020-06-24 11:38:37 -0300

    add gameplayMain to timetobeat

--- commands/timetobeat.js
+++ commands/timetobeat.js
@@ -1,9 +1,19 @@
+let hltb = require("howlongtobeat");
+let hltbService = new hltb.HowLongToBeatService();
+
 module.exports = {
-	name: 'timetobeat',
-	description: 'Shows the time to beat based on howlongtobeat.com',
-	execute(msg, args) {
-		if (!args.length) {
-			return msg.reply(`What console?`);
-		}
-	},
+  name: "timetobeat",
+  description: "Shows the time to beat based on howlongtobeat.com",
+  execute(msg, args) {
+    if (!args.length) {
+      return msg.reply(`What game?`);
+    } else {
+      var gam = args.toString().replace(/,/g, " ");
+      hltbService.search(gam).then((response) => {
+		console.log(response);
+		const main = response.data[0].gameplayMain;
+		console.log(main)
+      });
+    }
+  },
 };
